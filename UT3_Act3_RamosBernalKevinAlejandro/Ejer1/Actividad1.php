add35d0e2dcd63c1fbc7114645bbda40bf8df4bb
Kevin Ramos <182262851+Peti-sui@users.noreply.github.com>
2025-11-11 17:40:37 +0000

    Add files via upload

---
+++ UT3_Act3_RamosBernalKevinAlejandro/Ejer1/Actividad1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+
+<form method="post">
+
+    <label>Aceptas los terminos y condiciones??</label>
+    <input type="checkbox" name="terminos" value="acepto">
+    <button type="submit">Enviar</button>
+</form>
+    
+</body>
+</html>
