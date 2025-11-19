0d516dfae6fa05eda7eb0308920f31ffde573b17
Kevin Ramos <182262851+Peti-sui@users.noreply.github.com>
2025-11-19 21:48:28 +0000

    Add files via upload

---
+++ Act 2 UT3/UT3_Act1_RamosBernalKevinAlejandro/Ejer1/Ejercicio1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <form method="post">
+        <label for="tiempo">Crear una cookie con una duracion de 
+            <input type="number" name="tiempo"> segundos (entre 1 y 60)</label>
+        <button type="submit">Crear</button>   
+        <br>
+        <label for="comprobar">Comprobar la cookie
+            <button type="submit">Comprobar</button></label>
+        <br>
+        <label for="destruir">Destruir la cookie
+            <button type="submit">destruir</button></label>
+    </form>
+    
+</body>
+</html>
